9e4d6766d28dcb53b386403d1bf346d3f1bc4619
原口　善智 <ruge.e.azul@gmail.com>
2020-12-11 22:33:19 +0900

    ユーザーログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :phone_number,:contact_email,:contact_location])
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :phone_number,:contact_email,:contact_location,:prefecture_id])
 end
 end
