5581c426d1d4b11afa03b9e9515ed7ea16281ba3
原口　善智 <ruge.e.azul@gmail.com>
2020-12-13 16:12:22 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,18 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :quantity
+    validates :description
+    validates :deadline
+    validates :price
+    validates :contact_location
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :prefecture_id
+    validates :category_id
+    validates :condition_id
+  end
 end
