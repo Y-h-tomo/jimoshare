a0113c5e55d64c89ce43175f7e32e4519045d4af
原口　善智 <ruge.e.azul@gmail.com>
2020-12-14 11:50:18 +0900

    画像テストデプロイ

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,7 +9,27 @@ RSpec.describe Item, type: :model do
       it '全ての情報が正しく入力されていたら、商品が出品できる' do
         expect(@item).to be_valid
       end
-      it ''
+      it '全ての情報が正しく入力されており、商品名が40文字以内なら出品できる' do
+        @item.name = Faker::Lorem.characters(number: 40)
+        expect(@item).to be_valid
+      end
+      it '全ての情報が正しく入力されており、商品説明・条件が200文字以内なら出品できる' do
+        @item.description = Faker::Lorem.characters(number: 200)
+        expect(@item).to be_valid
+      end
+      it '全ての情報が正しく入力されており,個数が99個でも出品できる' do
+        @item.quantity  = 99
+        expect(@item).to be_valid
+      end
+      it '全ての情報が正しく入力されており,価格が0円でも出品できる' do
+        @item.price = 0
+        expect(@item).to be_valid
+      end
+      it '全ての情報が正しく入力されており,価格が999_999円でも出品できる' do
+        @item.price = 999_999
+        expect(@item).to be_valid
+      end
+
     end
   end
 end
