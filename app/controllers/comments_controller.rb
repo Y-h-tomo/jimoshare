d74320eeca71f93ec63984cb15ee128206b303f3
原口　善智 <ruge.e.azul@gmail.com>
2020-12-20 00:17:16 +0900

    テスト修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,10 +14,9 @@ class CommentsController < ApplicationController
     end
   end
 
-
   private
-  def comment_params
-    params.require(:comment).permit(:text).merge(user_id: current_user.id,item_id: params[:item_id])
-  end
 
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
