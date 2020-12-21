aa0838f00d4446e07d5a7895e1a1e69610e31722
原口　善智 <ruge.e.azul@gmail.com>
2020-12-21 20:54:29 +0900

    rubocop

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,24 +6,24 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
     authorization
-   end
+  end
 
-   def google_oauth2
+  def google_oauth2
     authorization
-   end
+  end
 
-   private
+  private
 
-   def authorization
-     @user = User.from_omniauth(request.env["omniauth.auth"])
-     @user = sns_info[:user]
-     if @user.persisted? #ユーザー情報が登録済みなので、新規登録ではなくログイン処理を行う
+  def authorization
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    @user = sns_info[:user]
+    if @user.persisted? # ユーザー情報が登録済みなので、新規登録ではなくログイン処理を行う
       sign_in_and_redirect @user, event: :authentication
-    else #ユーザー情報が未登録なので、新規登録画面へ遷移する
+    else # ユーザー情報が未登録なので、新規登録画面へ遷移する
       @sns_id = sns_info[:sns].id
       render template: 'devise/registrations/new'
     end
-   end
+  end
   # You should also create an action method in this controller like this:
   # def twitter
   # end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   belongs_to_active_hash :prefecture
   has_many :items, dependent: :destroy
   has_many :tickets, dependent: :destroy
@@ -36,18 +36,19 @@ class User < ApplicationRecord
     clean_up_passwords
     result
   end
+
   def self.from_omniauth(auth)
     sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
-       # sns認証したことがあればアソシエーションで取得
-   # 無ければemailでユーザー検索して取得orビルド(保存はしない)
-   user = User.where(email: auth.info.email).first_or_initialize(
-    nickname: auth.info.name,
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
       email: auth.info.email
-  )
-  if user.persisted?
-    sns.user = user
-    sns.save
-  end
-  { user: user, sns: sns }
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
   end
 end
