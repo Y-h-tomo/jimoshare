2a51d0e2ebb140034863e961449d92cff644cd98
原口　善智 <ruge.e.azul@gmail.com>
2020-12-22 19:59:22 +0900

    動作チェック

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -18,7 +18,7 @@ class TicketsController < ApplicationController
     item_user_ban
     over_ban
     num = Faker::Number.number(digits: 6)
-    @ticket = Ticket.new(
+    @ticket = Ticket.create(
       number: num,
       item_id: params[:item_id],
       user_id: current_user.id
